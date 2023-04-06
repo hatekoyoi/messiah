eed67b2e4fd6cb6d1a3b1b120a34860be4407157
ky0he1 <kyouta1218@gmail.com>
2023-04-06 13:03:14 +0900

    ループに対応

--- src/emulator_function.h
+++ src/emulator_function.h
@@ -5,6 +5,12 @@
 
 #include "emulator.h"
 
+// EFLAGのビットフラグ
+#define CARRY_FLAG (1)
+#define ZERO_FLAG (1 << 6)
+#define SIGN_FLAG (1 << 7)
+#define OVERFLOW_FLAG (1 << 11)
+
 // プログラムカウンタから相対位置にある符号無し8bit値を取得
 uint32_t get_code8(Emulator* emu, int index);
 
@@ -35,8 +41,26 @@ void set_memory8(Emulator* emu, uint32_t address, uint32_t value);
 // メモリのindex番地に32bit値を設定する
 void set_memory32(Emulator* emu, uint32_t address, uint32_t value);
 
+// スタックに32bitの値を積む
 void push32(Emulator* emu, uint32_t value);
 
+// スタックから32bitの値を取り出す
 uint32_t pop32(Emulator* emu);
+
+// EFLAGの各フラグの設定
+void set_carry(Emulator* emu, int is_carry);
+void set_zero(Emulator* emu, int is_carry);
+void set_sign(Emulator* emu, int is_carry);
+void set_overflow(Emulator* emu, int is_carry);
+
+// EFLAGの各フラグの取得
+int is_carry(Emulator* emu);
+int is_zero(Emulator* emu);
+int is_sign(Emulator* emu);
+int is_overflow(Emulator* emu);
+
+// 減算によるEFLAGの更新
+void update_eflags_sub(Emulator* emu, uint32_t v1, uint32_t v2,
+                       uint64_t result);
 
 #endif
--- src/test.c
+++ src/test.c
@@ -1,3 +1,26 @@
-int add(int a, int b) { return a + b; }
+// int sum(int a, int b) {
+//     int sum = 0;
+//     goto cond;
+// loop:
+//     sum += a;
+//     a++;
+// cond:
+//     if (a <= b) {
+//         goto loop;
+//     }
+//     return sum;
+// }
 
-int main(void) { return add(2, 5); }
+// int main(void) { return sum(1, 10); }
+
+int sum(int a, int b) {
+    int sum;
+    sum = 0;
+    while (a <= b) {
+        sum += a;
+        a++;
+    }
+    return sum;
+}
+
+int main(void) { return sum(1, 10); }
